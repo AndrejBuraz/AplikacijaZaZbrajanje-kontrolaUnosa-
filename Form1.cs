1c91fc76b3357c8f9e3eb12607d38e8a014cc0f8
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2022-09-22 19:55:48 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Moja2WindowsAplikacija
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonIzracun_Click(object sender, EventArgs e)
+        {
+            int brojA, brojB, zbroj = 0, iznimka = 1;
+            try
+            {
+                brojA = Convert.ToInt32(unosBrojaA.Text);
+                zbroj = brojA;
+            }
+            catch
+            {
+                unosBrojaA.Text = "Pogrešan unos, upišite broj";
+                iznimka = 0;
+            }
+            try
+            {
+                brojB = Convert.ToInt32(unosBrojaB.Text);
+                zbroj += brojB;
+            }
+            catch
+            {
+                unosBrojaB.Text = "Pogrešan unos, upišite broj";
+                iznimka = 0;
+            }
+            
+
+            rezultat.Text = "" + (zbroj * iznimka);
+        }
+
+        private void unosBrojaB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
